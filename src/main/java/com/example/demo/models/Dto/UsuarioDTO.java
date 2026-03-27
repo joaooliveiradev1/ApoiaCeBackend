7d400c81c4c829988b751da48e7bc319387f7658
Joao Oliveira <joaooliveiradev1@gmail.com>
2026-03-27 19:27:01 -0300

    Feat: Add model de usuario, repository, dtos, service e controller + auth jwt / jwt filter

---
+++ src/main/java/com/example/demo/models/Dto/UsuarioDTO.java
@@ -0,0 +1,19 @@
+package com.example.demo.models.Dto;
+
+import com.example.demo.models.Enums.UsuarioRole;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+public class UsuarioDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private UsuarioRole role;
+}
---
+++ src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.models.Entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCpf(String cpf);
+}
